6a665858091a7f312e39c002522a4e459a4a6316
christie <78717070+imada4911@users.noreply.github.com>
2024-02-13 13:36:57 +0900

    反映します

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { M_PLUS_1p } from "next/font/google";
+import "./globals.css";
+import TopMenu from "@/components/TopMenu";
+import SideMenu from "@/components/SideMenu";
+
+const inter = M_PLUS_1p({ subsets: ["latin"], weight: ["400", "500", "700"] });
+
+export const metadata = {
+  title: "OriginDoc",
+  description: "This is a OriginDoc site",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        <main className="flex-row flex">
+          <SideMenu />s
+          <section className="pl-[300px] max-lg:pl-[146px] max-md:pl-[0] w-ful min-h-screen overflow-hidden">
+            <TopMenu />
+            {children}
+          </section>
+        </main>
+      </body>
+    </html>
+  );
+}
